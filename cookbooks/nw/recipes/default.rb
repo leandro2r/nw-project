2e43c4aa3f6bb89c545bd6266e8812ca3dd84c43
Leandro Rodrigues <leandro.l2r@gmail.com>
2019-07-29 23:52:32 +0000

    Fix nw recipe and role

--- cookbooks/nw/recipes/default.rb
+++ cookbooks/nw/recipes/default.rb
@@ -17,6 +17,16 @@ cookbook_file '/usr/local/bin/nw' do
   mode '0755'
 end
 
+if  node['nw']['swarm'] == 'init'
+  execute 'Docker swarm init' do
+    command "docker swarm init --advertise-addr #{node['nw']['swarm_init']}"
+  end
+else
+  execute 'Docker swarm join' do
+    command "docker swarm join --token #{node['nw']['token']} #{node['nw']['swarm_init']}:2377"
+  end
+end
+
 service 'nw.service' do
   action :start
   supports enable: true, start: true, stop: true, restart: true
--- roles/master.rb
+++ roles/master.rb
@@ -3,3 +3,10 @@ description 'Master Role'
 
 run_list 'recipe[docker]',
          'recipe[nw]'
+
+override_attributes(
+  'nw' => {
+    'swarm' => 'init',
+    'swarm_init' => '11.12.13.14'
+  }
+)
