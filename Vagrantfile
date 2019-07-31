7aeeabaa89c48acedf3e3e9518867dd3a13bf405
Leandro <leandro.l2r@gmail.com>
2019-07-31 19:47:39 -0300

    Fix indentation of all recipes and some other files

--- Vagrantfile
+++ Vagrantfile
@@ -5,38 +5,38 @@
 Vagrant::DEFAULT_SERVER_URL.replace('https://vagrantcloud.com')
 
 Vagrant.configure('2') do |config|
-    config.vm.box = 'ubuntu/bionic64'
+  config.vm.box = 'ubuntu/bionic64'
 
-    config.ssh.insert_key = false
+  config.ssh.insert_key = false
 
-    config.vm.provider 'virtualbox' do |vb|
-        vb.memory = '512'
-        vb.cpus = '1'
-    end
+  config.vm.provider 'virtualbox' do |vb|
+    vb.memory = '512'
+    vb.cpus = '1'
+  end
 
-    config.vm.synced_folder "./", "/tmp/swarm"
-    config.vm.box_check_update = false
+  config.vm.synced_folder "./", "/tmp/swarm"
+  config.vm.box_check_update = false
 
-    (1..2).each do |i|
-        config.vm.define "node#{i}" do |node|
-            node.vm.hostname = "n#{i}"
-            node.vm.network "private_network", ip: "11.12.13.#{i}", auto_config: false
-            # node.vm.network 'forwarded_port', guest: 80, host: 80
+  (1..2).each do |i|
+    config.vm.define "node#{i}" do |node|
+      node.vm.hostname = "n#{i}"
+      node.vm.network "private_network", ip: "11.12.13.#{i}", auto_config: false
+      # node.vm.network 'forwarded_port', guest: 80, host: 80
 
-            node.vm.provision :chef_zero do |chef|
-                chef.cookbooks_path = 'cookbooks'
-                chef.provisioning_path = "/tmp/vagrant-chef-#{1}"
-                chef.roles_path = 'roles'
-                chef.nodes_path = 'nodes'
+      node.vm.provision :chef_zero do |chef|
+        chef.cookbooks_path = 'cookbooks'
+        chef.provisioning_path = "/tmp/vagrant-chef-#{1}"
+        chef.roles_path = 'roles'
+        chef.nodes_path = 'nodes'
 
-                if i == 1
-                    chef.add_role('master')
-                else
-                    chef.add_role('node')
-                end
-
-                chef.custom_config_path = 'custom.chef'
-            end
+        if i == 1
+          chef.add_role('master')
+        else
+          chef.add_role('node')
         end
+
+        chef.custom_config_path = 'custom.chef'
+      end
     end
+  end
 end
--- cookbooks/nw/recipes/default.rb
+++ cookbooks/nw/recipes/default.rb
@@ -1,54 +1,54 @@
 package 'build-essential'
 
 directory '/opt/nw-project' do
-    recursive true
+  recursive true
 end
 
 cookbook_file '/opt/nw-project/docker-compose.yml' do
-    source 'docker-compose.yml'
+  source 'docker-compose.yml'
 end
 
 cookbook_file '/etc/systemd/system/nw.service' do
-    source 'extras/system/nw.service'
+  source 'extras/system/nw.service'
 end
 
 cookbook_file '/usr/local/sbin/nw' do
-    source 'extras/daemon/nw'
-    mode '0755'
+  source 'extras/daemon/nw'
+  mode '0755'
 end
 
 if node['nw']['swarm'] == 'join'
-    TOKEN = `cat /tmp/swarm/swarm.token`.strip
-    SWARM_INIT = `cat /tmp/swarm/ip.txt`.strip
+  TOKEN = `cat /tmp/swarm/swarm.token`.strip
+  SWARM_INIT = `cat /tmp/swarm/ip.txt`.strip
 
-    file '/tmp/swarm/swarm.token' do
-        action :nothing
-    end
+  file '/tmp/swarm/swarm.token' do
+    action :nothing
+  end
 
-    file '/tmp/swarm/ip.txt' do
-        action :nothing
-    end
+  file '/tmp/swarm/ip.txt' do
+    action :nothing
+  end
 
-    execute 'Docker swarm join' do
-        command "docker swarm join --token #{TOKEN} #{SWARM_INIT}:2377"
-        notifies :delete, 'file[/tmp/swarm/swarm.token]', :delayed
-        notifies :delete, 'file[/tmp/swarm/ip.txt]', :delayed
-    end
+  execute 'Docker swarm join' do
+    command "docker swarm join --token #{TOKEN} #{SWARM_INIT}:2377"
+    notifies :delete, 'file[/tmp/swarm/swarm.token]', :delayed
+    notifies :delete, 'file[/tmp/swarm/ip.txt]', :delayed
+  end
 else
-    execute 'Docker swarm init' do
-        command "docker swarm init --advertise-addr #{node['ipaddress']}"
-    end
+  execute 'Docker swarm init' do
+    command "docker swarm init --advertise-addr #{node['ipaddress']}"
+  end
 
-    execute 'Add ipaddress to share' do
-        command "echo #{node['ipaddress']} > /tmp/swarm/ip.txt"
-    end
+  execute 'Add ipaddress to share' do
+    command "echo #{node['ipaddress']} > /tmp/swarm/ip.txt"
+  end
 
-    execute 'Add swarm token to share' do
-        command 'docker swarm join-token -q manager > /tmp/swarm/swarm.token'
-    end
+  execute 'Add swarm token to share' do
+    command 'docker swarm join-token -q manager > /tmp/swarm/swarm.token'
+  end
 end
 
 service 'nw.service' do
-    action :start
-    supports enable: true, start: true, stop: true, restart: true
+  action :start
+  supports enable: true, start: true, stop: true, restart: true
 end
